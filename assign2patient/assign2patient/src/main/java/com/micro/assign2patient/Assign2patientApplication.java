9b6328e4e012dadf5d274a0f878483a198f09c15
prakash6007 <giriprakash63040@gmail.com>
2026-02-26 23:56:14 +0530

    Add files via upload

---
+++ assign2patient/assign2patient/src/main/java/com/micro/assign2patient/Assign2patientApplication.java
@@ -0,0 +1,13 @@
+package com.micro.assign2patient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assign2patientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assign2patientApplication.class, args);
+	}
+
+}
---
+++ assign2patient/assign2patient/src/test/java/com/micro/assign2patient/Assign2patientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.assign2patient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assign2patientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
