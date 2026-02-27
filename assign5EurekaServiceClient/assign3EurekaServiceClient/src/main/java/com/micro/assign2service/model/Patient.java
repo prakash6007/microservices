9ad9a4629746690381bc4267cf0d13f13fa03095
prakash6007 <giriprakash63040@gmail.com>
2026-02-27 22:35:45 +0530

    Add files via upload

---
+++ assign5EurekaServiceClient/assign3EurekaServiceClient/src/main/java/com/micro/assign2service/model/Patient.java
@@ -0,0 +1,48 @@
+package com.micro.assign2service.model;
+
+
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "patients")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+
+    @ManyToMany
+    @JoinTable(
+            name = "patient_services",
+            joinColumns = @JoinColumn(name = "patient_id"),
+            inverseJoinColumns = @JoinColumn(name = "service_id")
+    )
+    @JsonIgnoreProperties("patients")
+    private List<Service> services = new ArrayList<>();
+
+    public Patient() {}
+
+    public Patient(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public Long getId() { return id; }
+    public String getFirstname() { return firstname; }
+    public String getLastname() { return lastname; }
+    public List<Service> getServices() { return services; }
+
+    public void setId(Long id) { this.id = id; }
+    public void setFirstname(String firstname) { this.firstname = firstname; }
+    public void setLastname(String lastname) { this.lastname = lastname; }
+    public void setServices(List<Service> services) { this.services = services; }
+}
---
+++ assign5EurekaServiceClient/assign3EurekaServiceClient/src/test/java/com/micro/assign3EurekaServiceClient/Assign3EurekaServiceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro.assign3EurekaServiceClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assign3EurekaServiceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
